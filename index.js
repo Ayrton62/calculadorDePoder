322039afcd8051d8c71526611384ad1b88f1b634
Ayrtoon <ayrtoon2santos@gmail.com>
2022-06-06 21:35:08 -0300

    agrego css

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ switch(entrenado){
     case "goku":
         alert("Genial, el poder de Goku es de 500000 por lo que puede entrenar el tiempo que desees")
         time = parseInt(prompt("¿Por cuantos dias te gustaria que entrene " + entrenado + " en esta ocasion?"))
-            habitacionTiempo = entrenamiento(goku, time)
+        habitacionTiempo = entrenamiento(goku, time)
         alert("Increible, el nuevo poder de Goku es de " + habitacionTiempo)
         break;
     case "gohan":
         alert("Genial, el poder de Gohan es de 270000 por lo que puede entrenar hasta 15 dias")
         time = parseInt(prompt("¿Por cuantos dias te gustaria que entrene " + entrenado + " en esta ocasion?"))
-    while ( time > 15 ){
-        time = parseInt(prompt("Eso es demasiado tiempo para Gohan, es muy arriesgado, Gohan puede entrenar hasta 15 dias"))
+        while ( time > 15 ){
+            time = parseInt(prompt("Eso es demasiado tiempo para Gohan, es muy arriesgado, Gohan puede entrenar hasta 15 dias"))
         }
-            habitacionTiempo = entrenamiento(gohan, time)
+        habitacionTiempo = entrenamiento(gohan, time)
         alert("Increible, el nuevo poder de Gohan es de " + habitacionTiempo)
         break;
     default:
