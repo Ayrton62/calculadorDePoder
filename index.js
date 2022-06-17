61c4e29cccb22d94fab75b3d8c5199090dfaa1ad
Ayrtoon <ayrtoon2santos@gmail.com>
2022-06-16 21:01:23 -0300

    correccion

--- index.js
+++ index.js
@@ -7,13 +7,22 @@ class Personaje {
  }
 };
 const personajes =[
-{ "Goku" 40 "Saiyan" 500000 } 
-{"Vegeta" 46, "Saiyan", 495000 }
-{"Gohan" 24 " Humano - Saiyan " 270000}
-]
+{   nombre: "Goku",
+    edad: 40, 
+    raza: "Saiyan", 
+    poder: 500000 }, 
+{   nombre: "Vegeta",
+    edad: 46, 
+    raza: "Saiyan", 
+    poder: 495000 },
+{   nombre: "Gohan",
+    edad: 21, 
+    raza: "Saiyan - Humano", 
+    poder: 215000 }
+]     
 
 const entrenamiento = (Personaje, time) => {
-    return  * 1.05 * time;
+    return Personaje.poder * 1.05 * time;
 }
 
 let entrenado = prompt("¿A quién te gustaria entrenar ahora? Podrias probar con Goku, con Vegeta o con Gohan, pronto habra mas guerreros").toLowerCase();
@@ -27,7 +36,7 @@ switch(entrenado){
         while ( time > 25 ){
             time = parseInt(prompt("Eso es demasiado tiempo para Goku, no lo salvan ni las semillas del hermitaneo, Goku puede entrenar hasta 15 dias"))
         }
-        habitacionTiempo = entrenamiento(goku, time)
+        habitacionTiempo = entrenamiento(personajes.find(p => p.nombre == "Goku"), time)
         alert("Increible, el nuevo poder de Goku es de " + habitacionTiempo)
         break;
     case "vegeta":
@@ -36,16 +45,16 @@ switch(entrenado){
         while ( time > 25 ){
             time = parseInt(prompt("Eso es demasiado tiempo para Vegeta, ni siquiera el principe de lo saiyan se banca tanto, Vegeta puede entrenar hasta 15 dias"))
         }
-        habitacionTiempo = entrenamiento(vegeta, time)
+        habitacionTiempo = entrenamiento(personajes.find(p => p.nombre == "Vegeta"), time)
         alert("Increible, el nuevo poder de Vegeta es de " + habitacionTiempo)
         break;
     case "gohan":
-        alert("Genial, el poder de Gohan es de 270000 por lo que puede entrenar hasta 15 dias")
+        alert("Genial, el poder de Gohan es de 215000 por lo que puede entrenar hasta 15 dias")
         time = parseInt(prompt("¿Por cuantos dias te gustaria que entrene " + entrenado + " en esta ocasion?"))
         while ( time > 15 ){
             time = parseInt(prompt("Eso es demasiado tiempo para Gohan, acordate que gohan ahora estudia, Gohan puede entrenar hasta 15 dias"))
         }
-        habitacionTiempo = entrenamiento(gohan, time)
+        habitacionTiempo = entrenamiento(personajes.find(p => p.nombre == "Gohan"), time)
         alert("Increible, el nuevo poder de Gohan es de " + habitacionTiempo)
         break;
     default:
