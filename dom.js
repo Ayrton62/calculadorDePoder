c2d88bbce2a9217bd921eb897018ad2ffa92e59f
Ayrtoon <ayrtoon2santos@gmail.com>
2022-06-23 23:14:01 -0300

    evento

--- dom.js
+++ dom.js
@@ -1,17 +1,26 @@
 const mainCont = document.querySelector(".cont");
-for(let p of personajes){
-const cont__pj = document.createElement('div');
-cont__pj.classList.add("cont__pj");
+for (let p of personajes) {
+    const cont__pj = document.createElement('div');
+    cont__pj.classList.add("cont__pj");
 
-const title__pj = document.createElement('h4');
-title__pj.classList.add("title__pj");
-title__pj.innerText = p.name;
+    const title__pj = document.createElement('h4');
+    title__pj.classList.add("title__pj");
+    title__pj.innerText = p.name;
 
-const fotopj = document.createElement("img");
-fotopj.classList.add("fotopj");
-fotopj.setAttribute('src' , p.img);
+    const fotopj = document.createElement("img");
+    fotopj.classList.add("fotopj");
+    fotopj.setAttribute('src', p.img);
 
-cont__pj.appendChild(title__pj);
-cont__pj.appendChild(fotopj);
+    cont__pj.appendChild(title__pj);
+    cont__pj.appendChild(fotopj);
 
-mainCont.appendChild(cont__pj)};
+    mainCont.appendChild(cont__pj)
+};
+const boton = document.getElementsByClassName("btn")
+
+boton.addEventListener('onclick', pregunta)
+
+function pregunta() {
+    prompt("Quien entrenara?")
+}
+
--- index.js
+++ index.js
@@ -30,44 +30,8 @@ const entrenamiento = (Personaje, time) => {
     return Personaje.power * 1.05 * time;
 }
 
-let trained = prompt("¿A quién te gustaria entrenar ahora? Podrias probar con Goku, con Vegeta o con Gohan, pronto habra mas guerreros").toLowerCase();
 let time = 0;
 let habitacionTiempo = 0;
-
-switch (entrenado) {
-    case "goku":
-        alert("Genial, el poder de Goku es de 500000 por lo que puede entrenar hasta 25 dias")
-        time = parseInt(prompt("¿Por cuantos dias te gustaria que entrene " + trained + " en esta ocasion?"))
-        while (time > 25) {
-            time = parseInt(prompt("Eso es demasiado tiempo para Goku, no lo salvan ni las semillas del hermitaneo, Goku puede entrenar hasta 15 dias"))
-        }
-        habitacionTiempo = entrenamiento(personajes.find(p => p.name == "Goku"), time)
-        alert("Increible, el nuevo poder de Goku es de " + habitacionTiempo)
-        break;
-    case "vegeta":
-        alert("Genial, el poder de Vegeta es de 495000 por lo que puede entrenar hasta 25 dias")
-        time = parseInt(prompt("¿Por cuantos dias te gustaria que entrene " + trained + " en esta ocasion?"))
-        while (time > 25) {
-            time = parseInt(prompt("Eso es demasiado tiempo para Vegeta, ni siquiera el principe de lo saiyan se banca tanto, Vegeta puede entrenar hasta 15 dias"))
-        }
-        habitacionTiempo = entrenamiento(personajes.find(p => p.name == "Vegeta"), time)
-        alert("Increible, el nuevo poder de Vegeta es de " + habitacionTiempo)
-        break;
-    case "gohan":
-        alert("Genial, el poder de Gohan es de 215000 por lo que puede entrenar hasta 15 dias")
-        time = parseInt(prompt("¿Por cuantos dias te gustaria que entrene " + trained + " en esta ocasion?"))
-        while (time > 15) {
-            time = parseInt(prompt("Eso es demasiado tiempo para Gohan, acordate que gohan ahora estudia, Gohan puede entrenar hasta 15 dias"))
-        }
-        habitacionTiempo = entrenamiento(personajes.find(p => p.name == "Gohan"), time)
-        alert("Increible, el nuevo poder de Gohan es de " + habitacionTiempo)
-        break;
-    default:
-        alert("Por favor, no repitas ese nombre, no es digno de la sala del tiempo")
-        break;
-};
-alert("Suficiente entrenamiento por ahora, regresa en un rato")
-
 
 
 
